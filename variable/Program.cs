136fc017a48d782ffc070f481bc8fcd2743ad051
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:40:37 +0600

    first commit

---
+++ variable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int a = 10;
+            // Byte b = a;
+            //Byte a = 10;
+            //int b = a;
+            int a = 10;
+            Byte b = (Byte)a;
+            Byte c = Convert.ToByte(a);
+
+       
+
+
+        }
+    }
+}
